112bbab5617cfcb8a3b3ad6e9db01ca9d2dceef6
Victor Duran <victor.duran.em@gmail.com>
2020-04-21 04:58:45 +0000

    Adding diameter_binary_tree

---
+++ diameter_binary_tree/src/main.rs
@@ -0,0 +1,29 @@
+// Definition for a binary tree node.
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+
+use std::rc::Rc;
+use std::cell::RefCell;
+
+pub fn diameter_of_binary_tree(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    0
+}
+
+pub fn main() {
+
+}
