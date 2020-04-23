160c23e6dc274ce3da85cb3c875685db4d644c90
Victor Duran <victor.duran.em@gmail.com>
2020-04-23 04:32:27 +0000

    Adding second attempt for contiguous array

--- contiguous_array/src/main.rs
+++ contiguous_array/src/main.rs
@@ -1,57 +1,48 @@
+use std::collections::HashMap;
+
 fn main() {
-    let vec = vec![0,1,1,1,1,0,0];
+    let vec = vec![1,0,1,1,1,1,0,0,1,0,1,0,0,1,0,1,1,1];
     println!("{}", find_max_length(vec));
 }
 
 pub fn find_max_length(nums: Vec<i32>) -> i32 {
 
+    // HashMap<position, first_index_in_position>
+    let mut map: HashMap<i32, usize> = HashMap::new();
+    map.insert(0, 0);
+
     if nums.len() < 2 {
         return 0;
     }
 
+    let mut position: i32 = 0;
     let mut max: usize = 0;
-    let mut starting_position: usize = 0;
 
-    loop {
-
-        max = find_max_length_single_iteration(&mut max, &mut starting_position, &nums);
-
-        if starting_position == nums.len() - 2 {
-            break;
+    for (i, num) in nums.iter().enumerate() {
+        if *num == 1 {
+            position += 1;
+        } else {
+            position -= 1;
         }
+        // println!("Iteration: {}", i);
+        // println!("Map before loop: {:?}", map);
+        // println!("Position: {}", position);
+        if map.contains_key(&position) {
+            let distance: usize = if i == 1 { 2 } else { i - map.get(&position).unwrap() };
+            // println!("Position found in the map");
+            // println!("Distance from first position: {}", distance);
+            if distance > max {
+                max = distance;
+            }
 
-        starting_position += 1;
+        } else {
+            // println!("Position not found in the map");
+            map.insert(position, i);
+        }
+        // println!("Max: {}", max);
+        // println!("Map after loop {:?}", map);
+        // println!("");
     }
 
     max as i32
-}
-
-pub fn find_max_length_single_iteration(max: &mut usize, starting_position: &mut usize, nums: &Vec<i32>) -> usize {
-    let mut sum: i32 = 0;
-
-    // println!("{:?}", *nums);
-    for i in *starting_position..nums.len() {
-
-        // println!("Value of i: {}", i);
-        let num = nums[i as usize];
-        
-        if num == 1 {
-            sum += 1;
-        } else {
-            sum -= 1;
-        }
-
-        // println!("Value of sum: {}", sum);
-
-        let max_found = (i + 1) - *starting_position;
-
-        if sum == 0 && max_found > *max {
-            *max = max_found;
-        }
-
-        // println!("Value of max {}", max);
-        // println!("");
-    }
-    
-    *max
 }
